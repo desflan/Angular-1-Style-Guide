d92bbaaca0038b12f2d49782aa4795b8e41a76ec
Des Flanagan <desflanagan@hotmail.com>
2016-11-25 16:59:24 +1100

    added dropdown control for filtering

--- AngularDemo/app/view/viewController.js
+++ AngularDemo/app/view/viewController.js
@@ -11,79 +11,92 @@
 
         /* jshint validthis:true */
         var vm = this;
-        vm.title = 'View Activities';
+        vm.title;
         vm.activities;
         vm.success;
         vm.error;
         vm.gridOptions;
+        vm.activityTypeChanged;
+        vm.selectedItem;
 
         activate();
 
         function activate() {
 
-            vm.name = "activity";
+            vm.title = 'View Activities';
+            vm.activities = [
+                { name: "Royal National Park", type: "Cycle", start: "01/11/2016 6:10AM", duration: "3 hours 16 mins", distance: "112.5km", comments: "hill workout" },
+                { name: "Bondi to Coogee", type: "Run", start: "05/11/2016 7:13PM", duration: "42 mins", distance: "11.6km", comments: "focus on speed" }
+            ];
+            vm.activityTypes = [{ Id: 1, Name: "None" },{ Id: 2, Name: "Run" }, { Id: 3, Name: "Cycle" }, { Id: 4, Name: "Swim" }];
+            vm.activityTypeChanged = activityTypeChanged;
+            vm.selectedItem = vm.activityTypes[0].Name;
             vm.success = false;
             vm.error = false;
             vm.gridOptions = setGridOptions();
-            vm.activities = [
-                { name: "Royal National Park", type: "Cycling", start: "01/11/2016 6:10AM", duration: "3 hours 16 mins", distance: "112.5km", comments: "hill workout" },
-                { name: "Bondi to Coogee", type: "Run", start: "05/11/2016 7:13PM", duration: "42 mins", distance: "11.6km", comments: "focus on speed" }
-            ];
+        }
+
+
+        function setGridOptions() {
+            return {
+                data: 'vm.activities',
+                enableGridMenu: true,
+                enableRowHeaderSelection: false,
+                multiSelect: false,
+                rowSelection: true,
+                showGridFooter: false,
+                columnDefs: [
+                    {
+                        field: 'name',
+                        displayName: 'Name',
+                        enableSorting: true,
+                        enableHiding: true,
+                        width: '20%'
+                    },
+                    {
+                        field: 'type',
+                        displayName: 'Type',
+                        enableSorting: true,
+                        enableHiding: true,
+                        width: '10%'
+                    },
+                    {
+                        field: 'start',
+                        displayName: 'Start',
+                        enableSorting: true,
+                        enableHiding: true,
+                        width: '20%'
+                    },
+                    {
+                        field: 'duration',
+                        displayName: 'Duration',
+                        enableSorting: true,
+                        enableHiding: true,
+                        width: '20%'
+                    },
+                    {
+                        field: 'distance',
+                        displayName: 'Distance',
+                        enableSorting: true,
+                        enableHiding: true,
+                        width: '10%'
+                    },
+                    {
+                        field: 'comments',
+                        displayName: 'Comments',
+                        enableSorting: true,
+                        enableHiding: true,
+                        width: '20%'
+                    }
+                ]
+            };
+        }
+
+
+        function activityTypeChanged(type) {
+            vm.selectedItem = type;
         }
     }
 
-    function setGridOptions() {
-        return {
-            data: 'vm.activities',
-            enableGridMenu: true,
-            enableRowHeaderSelection: false,
-            multiSelect: false,
-            rowSelection: true,
-            showGridFooter: false,
-            columnDefs: [
-                {
-                    field: 'name',
-                    displayName: 'Name',
-                    enableSorting: true,
-                    enableHiding: true,
-                    width: '20%'
-                },
-                {
-                    field: 'type',
-                    displayName: 'Type',
-                    enableSorting: true,
-                    enableHiding: true,
-                    width: '10%'
-                },
-                {
-                    field: 'start',
-                    displayName: 'Start',
-                    enableSorting: true,
-                    enableHiding: true,
-                    width: '20%'
-                },
-                {
-                    field: 'duration',
-                    displayName: 'Duration',
-                    enableSorting: true,
-                    enableHiding: true,
-                    width: '20%'
-                },
-                {
-                    field: 'distance',
-                    displayName: 'Distance',
-                    enableSorting: true,
-                    enableHiding: true,
-                    width: '10%'
-                },
-                {
-                    field: 'comments',
-                    displayName: 'Comments',
-                    enableSorting: true,
-                    enableHiding: true,
-                    width: '20%'
-                }
-            ]
-        };
-    }
+    
 })();
