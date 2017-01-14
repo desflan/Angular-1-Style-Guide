a732482bdd242a70bf72e48d26a4c57bd7cea0b8
Des Flanagan <desflanagan@hotmail.com>
2017-01-14 13:05:42 +1100

    deleted unused code

--- AngularDemo/app/core/config.js
+++ AngularDemo/app/core/config.js
@@ -2,23 +2,6 @@
 
     'use strict';
 
-    //angular.module('app.core')
-    //.config(function (toastrConfig) {
-    //    angular.extend(toastrConfig, {
-    //        autoDismiss: false,
-    //        containerId: 'toast-container',
-    //        maxOpened: 1,
-    //        newestOnTop: true,
-    //        positionClass: 'toast-top-right',
-    //        preventDuplicates: false,
-    //        preventOpenDuplicates: false,
-    //        target: 'body',
-    //        timeOut: 5000,
-    //        extendedTimeout: '0',
-    //        closeButton: true
-    //    });
-    //});
-
     angular.module('app.core')
     .config(configure);
 
